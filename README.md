57f1f5d3928d520c8984a8d0499733c9a0040afa
Steven Jiang <jiangs1@students.rowan.edu>
2020-07-07 23:54:53 -0400

    functionality complete

--- README.md
+++ README.md
@@ -1,21 +1,30 @@
 # Web Scraper for medium.com
-### Description: This is a simple web scraper specifically written for medium.com articles.
+### Description: This is a simple web crawler and web scraper specifically written for medium.com articles. The crawler will begin with the user specified webpage and crawl to all other links on that page while adding the links to a single text file. After the crawling is complete, the scraping part begins. The scraper will attempt to retrieve the information specified further down in the README. Articles whose information don't exceed user specified parameters will not have their own designated text files with all the information. 
 ### Programming Language: Python
+
 ### Libraries Used:
     1. os
-        - For saving the scraped output into a text file.
-    2. requests
-        - For getting an http request to the webpage.
-    3. BeautifulSoup
+        - Saving the scraped output into a text file.
+    2. glob
+        - Deleting all text files from current directory.
+    3. requests
+        - Getting an http request to the webpage.
+    4. urlparse, urljoin
+        - Perform parsing and merging of url pieces.
+    5. BeautifulSoup
         - For getting the actual html of the webpage.
+
 ### Information Scraped:
     - Article's Title
     - Link
     - Number of Words in the Article
     - Number of "Claps" for the Article
+    - Plain text of the Article
+
 ### How to Use:
-    1. Open terminal and run "python webscraper.py" or run from your favorite python ide.
-    2. Paste the URL of the medium.com article that you wish to scrape information about.
-    3. In the current directory, a "data_1.txt" file should appear once the script finishes.
-    4. View the scraped information in the "data_1.txt" file.
-    5. Every time the script is run with a unique url, the script will automatically create a new 'data_#.txt' file, where # stands for a number.
+    1. Clone this repository.
+    2. Open terminal, locate the directory containing this project, and run "python webscraper.py" or run from your favorite python ide.
+    3. Paste the URL of the medium.com article that you wish to scrape information about, as well as providing parameters about the minimum word count and minimum clap count. 
+    4. In the current directory, a "all_url_links.txt" file should appear, which holds all urls that have been crawled, starting from the page given.
+    5. Additionally, pages whose info meets the specified parameters will have their own "data_*.txt" file, where * represents a number.
+    6. Every time the script is ran, all the previous .txt files will be deleted, so be sure to save them elsewhere before rerunning the script.
--- webscraper.py
+++ webscraper.py
@@ -1,14 +1,109 @@
 import os
+import glob
 import requests
+from urllib.request import urlparse, urljoin
 from bs4 import BeautifulSoup
-from itertools import islice
 
 
-#Use requests to get html and BeautifulSoup to parse it
+# global variables
+internal_urls = set()
+total_urls_visited = 0
+current_file_number = 1
+
+
+def is_valid(url: str) -> bool:
+    """
+    Checks whether the url (inside the href tag) is a valid link.
+
+    :param url: The url for the webpage
+    :return:    True if url is a valid link, False otherwise
+    """
+
+    # Parses the domain name of the url
+    domain = urlparse(url)
+    return bool(domain.netloc) and bool(domain.scheme)
+
+
+def get_all_website_links(url: str) -> list:
+    """
+    Gets all urls that are found on `url` webpage hosted on medium.com.
+
+    :param url: The url for the webpage
+    :return:    List of urls for the 'url' parameter
+    """
+
+    # All urls of `url` parameter
+    urls = set()
+
+    # Domain name of the url without the protocol (https)
+    domain_name = urlparse(url).netloc
+
+    # HTTP GET request to the url
+    result = requests.get(url)
+
+    # BeautifulSoup gets the html of the url's webpage
+    soup = BeautifulSoup(result.content, "html.parser")
+
+    # Begin looking for all links ie. <a href=""> on the current webpage
+    for a_tag in soup.findAll("a"):
+        href = a_tag.attrs.get("href")
+        
+        if href == "" or href is None:
+            # href empty tag, links nowhere
+            continue
+
+        # Join the url if it's relative (not absolute link)
+        href = urljoin(url, href)
+        # urlparse() returns a ParseResult object with [scheme, netloc, path, params, query]
+        parsed_href = urlparse(href)
+
+        # Combines url pieces to get ready to insert into final list of all the urls
+        href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
+
+        if not is_valid(href):
+            # Not a valid URL
+            continue
+        if href in internal_urls:
+            # Already in the set
+            # We only want unique url links
+            continue
+        if domain_name not in href:
+            # External link
+            continue
+
+        print(f"[*] Internal link: {href}")
+        urls.add(href)
+        internal_urls.add(href)
+
+    return urls
+
+
+def crawler(url: str, max_urls=50):
+    """
+    Crawls starting url web page and extracts all links. Performs recursive crawling on those links and repeat until base case.
+    
+    :params max_urls: Number of urls to crawl. This is to prevent infinite recursive calls and crash my/your computer :)
+    :return:          None
+    """
+
+    global total_urls_visited
+    total_urls_visited += 1
+
+    # First stores all the urls that are on the webpage given by the user
+    links = get_all_website_links(url)
+
+    # Loop through each url and recursively get all links on that page
+    for link in links:
+        # Specify a base case to exit recursion
+        if total_urls_visited > max_urls:
+            break
+        crawler(link, max_urls=max_urls)
+
+
 def getHTML(userURL):
-    # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
-    # cookie = {'Cookie': 'aws-priv=eyJ2IjoxLCJldSI6MCwic3QiOjB9; aws_lang=en; x-wl-uid=1qqB55cbZjrg3L/5ASidPnKLm7vU+Fspwr5i6FzpE/D/NQqNHucB8ZPeffvOAZgBWLXj5tPN3fV0=; session-id-time=2082787201l; session-id=132-3769011-5781220; ubid-main=133-0861431-6797846; i18n-prefs=USD; skin=noskin; session-token=vKMvwRImxeCZqtHjutETl/AHyKTdXfv40HfxcTVN5epzu2TOe7U6nOLREziRnhmu6NO4H1Fjr5UuCJvYW6C+gm9beB+yLdKPEIWHDRjE6MTXoXftNecZgyL4GsXqxghL5jdYtgbazsYtaYDuERsBRY4do+yIwtVJAJVQgxfo+mSdkqGaCdVN2Tx5wtLipXzI'}
-
+    """
+    
+    """
     try:
         # result = requests.get(url = resource, headers=headers, cookies=cookie)
         # Perform an HTTP GET request on the given url link
@@ -18,26 +113,56 @@ def getHTML(userURL):
         if result.status_code != 200:
             print('Webpage is not giving data: Error ', result.status_code)
             raise Exception
+            
+        src = result.content
+        soup = BeautifulSoup(src, 'lxml')
+        print('\nSuccessfully Parsed')
+        
+        # Returns the html of the webpage
+        print(type(soup))
+        return soup
     except:
         print('Something went wrong in getting the HTML code\n')
-        quit()
-
-    src = result.content
-    soup = BeautifulSoup(src, 'lxml')
-    print('\nSuccessfully Parsed')
+        return 
     
-    # Returns the html of the webpage
-    return soup
 
 
-def scraper(parsedPage, url):
+def scraper(url):
+    """
 
-    # Finds the first <h1> tag which seems to be a reasonable target for the article's title.
+    """
+    parsedPage = getHTML(url)
+
+    articleTitle = getArticleTitle(parsedPage)
+    wordCount = getWordCount(parsedPage)
+    clapCount = getClapCount(parsedPage)
+    articleText = getArticleText(parsedPage)
+    
+    wantedInfo = [articleTitle, url, wordCount, clapCount, articleText]
+
+    return wantedInfo
+
+
+def getArticleTitle(parsedPage):
+    """
+    # Finds the first <h1> tag which seems to be a reasonable target for the article's title.    
+    """
+    if parsedPage is None or parsedPage.find('h1') is None:
+        return ""
+    
     title = parsedPage.find('h1').text
+    return title
 
-    # Finds all the <p> tags with an "id" attribute.
-    # Not 100% accurate word count since this also grabs <p> tags not in the article.
-    # ie. "About the author" text may also be counted.
+
+def getWordCount(parsedPage):
+    """
+    Finds all the <p> tags with an "id" attribute.
+    Not 100% accurate word count since this also grabs <p> tags not in the article.
+    ie. "About the author" text may also be counted.
+    """
+    if parsedPage is None or parsedPage.find_all('p', {'id': True}) is None:
+        return 0
+
     wordList = parsedPage.find_all('p', {'id': True})
 
     textFromArticle = ""
@@ -45,9 +170,16 @@ def scraper(parsedPage, url):
     for i in range(len(wordList)):
         textFromArticle = textFromArticle + wordList[i].text + " "
 
-
     numWords = len(textFromArticle.split())
-    print("number of words: ", numWords)
+    return numWords
+
+
+def getClapCount(parsedPage):
+    """
+
+    """
+    if parsedPage is None or parsedPage.find_all('h4') is None:
+        return 0
 
     numClapsCandidates = parsedPage.find_all('h4')
     numClaps = 0
@@ -57,77 +189,125 @@ def scraper(parsedPage, url):
         if child is not None:
             numClaps = child.text
 
+    return numClaps
 
-    print(numClaps)
-    wantedInfo = [title, url, numWords, numClaps]
-    return wantedInfo
 
+def getArticleText(parsedPage):
+    """
+    Finds all the <p> tags with an "id" attribute.
+    Not 100% accurate word count since this also grabs <p> tags not in the article.
+    ie. "About the author" text may also be counted.
+    """
+    if parsedPage is None or parsedPage.find_all('p', {'id': True}) is None:
+        return ""
     
+    wordList = parsedPage.find_all('p', {'id': True})
+
+    textFromArticle = ""
+
+    for i in range(len(wordList)):
+        textFromArticle = textFromArticle + wordList[i].text + " "
+
+    return textFromArticle
+
+
 # Accepts a list of all the info that were scraped
 def saveTextFile(infoList):
 
     currentFileLocation = os.path.dirname(os.path.abspath(__file__))
 
-    # Variable to increment data file number ie. data_1.txt ---> data_2.txt
-    currentFileNumber = 1
+    # Global variable to increment data file number ie. data_1.txt ---> data_2.txt
+    global current_file_number
 
-    #Save it in html format
-    # myfile = open(currentFileLocation + '/data.txt', 'w')
-    filePath = currentFileLocation + '/data_' + str(currentFileNumber) + '.txt'
-    print("path testing: ", filePath)
-    myfile = open(filePath, 'w')
+    filePath = currentFileLocation + '/data_' + str(current_file_number) + '.txt'
 
-    # Clears content of the text file everytime script gets run
-    # myfile.truncate(0)
-
-    for item in infoList:
-        myfile.write("%s\n" % item)
+    with open(filePath, 'w', encoding="utf-8") as myfile:
+        for item in infoList:
+            myfile.write("%s\n" % item)
 
     myfile.close()
-    print('Successfully Written')
-    currentFileNumber = currentFileNumber + 1
+    current_file_number += 1
 
 
-# To prevent scraping webpages that have already been scraped.
-def check_url_unique(userURL):
-    # Geting directory path and setting up path for directory iteration.
+# Credit to Greenstick for the awesome progress bar code
+# Code: https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
+def main():    
+    url = "https://medium.com/shallow-thoughts-about-deep-learning/how-would-we-find-a-better-activation-function-than-relu-4409df217a5c"
+    minWords = 1000
+    minClaps = 30
+
+    # Change this to alter how much crawling should be done
+    # Misleading name, doesn't actually only crawl for specified number of max url
+    max_urls = 1
+
     directoryPath = os.path.dirname(os.path.abspath(__file__))
-    currentDirectory = str(directoryPath)
-    directory = os.fsencode(currentDirectory)
+    all_url_links_file = 'all_url_links.txt'
 
-    # Loop through every file in the current directory.
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
+    crawler(url, max_urls=max_urls)
 
-        # Only check the text files.
-        if filename.endswith(".txt"):
-            # Open the text file for reading.
-            with open(directoryPath + "/" + filename, 'r') as f:
-                # Gets the second line in the file which has the url.
-                urlFromFile = f.read().split('\n')[1]
+    print("[+] Total URL links:", len(internal_urls))
 
-            # Check scraped url with the url that the user just entered.
-            if urlFromFile == userURL:
-                # Getting here means that the url was found in the data.txt files
-                # Therefore, webpage has already been scraped before.
-                print("This webpage has already been scraped before!")
-                print("The info is stored in", filename, "!")
-                return False
-        else:
-            continue
-    # Return true, meaning user inputted url webpage hasn't been scraped before.
-    return True
-
-
-def main():
-    userURL = input('Please enter the link: ')
-    uniqueURL = check_url_unique(userURL)
+    # domain_name = urlparse(url).netloc
     
-    # Only perform webscraping on url's that haven't been scraped before.
-    if uniqueURL:
-        html = getHTML(userURL)
-        info = scraper(html, userURL)
-        saveTextFile(info)
+    # Save the internal links to a file
+    with open(all_url_links_file, 'w') as f:
+        for internal_link in internal_urls:
+            print(internal_link.strip(), file=f)
 
-# Call main method
+    for i in range(len(internal_urls)):
+        with open(directoryPath + '/' + all_url_links_file, 'r') as f:
+            # index = 1
+            printProgressBar(i+1, len(internal_urls), prefix='Progress', suffix='Complete', length=len(internal_urls))
+            # Gets the second line in the file which has the url.
+            # eachURL = f.read().split('\n')[line]
+            eachURL = f.read().split('\n')[i+1]
+
+            wantedInfo = scraper(eachURL)
+
+            number_of_words = wantedInfo[2]
+
+            # ie. turns "1.2K claps" into "1.2K"
+            claps_only_number = str(wantedInfo[3]).split()[0]
+            # ie. turns "1.2K" into "1200"
+            if claps_only_number[-1] == "K":
+                # [:-1] drops the last character from the string
+                number_of_claps = claps_only_number[:-1] * 1000
+            else:
+                number_of_claps = claps_only_number * 1000
+
+            if int(number_of_words) < minWords or int(number_of_claps) < minClaps:
+                # Article info does not meet specified requirement
+                continue
+            
+            # Making it here means that the article successfully met our criteria
+            # Saves all information into a text file
+            saveTextFile(wantedInfo)
+
+
+# Automatically removes every .txt file from the directory before beginning new scrape run
+for f in glob.glob("*.txt"):
+    os.remove(f)
+
 main()
